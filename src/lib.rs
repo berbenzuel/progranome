0f126923e6249c76bbc41dcf3e78f2a13d5bfb36
berbenzuel <zukovnik1010@gmail.com>
2026-02-22 22:49:21 +0100

    basic ui, buildable, can be run

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
 
+use slint::platform::SetPlatformError;
+use slint::Weak;
+
 slint::include_modules!();
 #[unsafe(no_mangle)]
-fn android_main(app: slint::android::AndroidApp) {
-    slint::android::init(app).unwrap();
-    MainWindow::new().unwrap().run().unwrap();
+pub fn android_main(app: slint::android::AndroidApp) -> Result<(),  Box<dyn std::error::Error>> {
+    slint::android::init(app)?;
+
+
+    let main = MainWindow::new()?;
+    let main_weak = main.as_weak();
+
+    // foo(main_weak)?;
+
+    main.run().or(Err(Box::from("MainWindow")))
 }
+
+// fn foo(weak: Weak<MainWindow>) -> Result<(), Box<dyn std::error::Error>> {
+//     let main_window = weak.upgrade().ok_or("error while handling window")?;
+//     main_window.global::<Logic>().set_numerator(10);
+//     Ok(())
+// }
+
